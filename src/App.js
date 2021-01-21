dd708a9952c8fe73b4682b6d5c1bfe7aa17cd69e
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-21 02:29:27 +0100

    Alert component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+import Alert from "./components/alert/Alert";
 import { BookForm } from "./components/BookForm";
 import { BookTable } from "./components/BookTable";
+import AlertState from "./context/alert/AlertState";
 import BookState from "./context/book/BookState";
 
 function App() {
   return (
     <BookState>
-      <div className="container App">
-        <h1>Add Book</h1>
-        <BookForm />
-        <BookTable />
-      </div>
+      <AlertState>
+        <div className="container ">
+          <Alert />
+          <h1 className="App">Add Book</h1>
+          <BookForm />
+          <BookTable />
+        </div>
+      </AlertState>
     </BookState>
   );
 }
---
+++ src/components/alert/Alert.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
+
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { alert } = alertContext;
+  return (
+    alert !== null && (
+      <div className={alert.type}>
+        <p>{alert.msg}</p>
+      </div>
+    )
+  );
+};
+
+export default Alert;
