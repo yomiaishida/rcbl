33822ed62d97dd7ae43ac9c949a1e383fa343711
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-21 14:25:59 +0100

    Removing book from local storage

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -4,7 +4,34 @@ import bookContext from "../context/book/bookContext";
 export const TableBody = ({ book }) => {
   const { removeBook } = useContext(bookContext);
 
+  const getBooks = () => {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  };
+
+  const removeBookFromLs = (isbn) => {
+    const books = getBooks();
+
     books.forEach(function (book, index) {
+      if (book.id === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  };
+
+  const onClick = () => {
+    removeBook(book.id);
+    removeBookFromLs(book.id);
+  };
+
   return (
     <tbody>
       <tr>
@@ -12,7 +39,7 @@ export const TableBody = ({ book }) => {
         <td>{book.author}</td>
         <td>{book.id}</td>
         <td>
-          <a href="#" className="delete" onClick={() => removeBook(book.id)}>
+          <a href="#" className="delete" onClick={onClick}>
             X
           </a>
         </td>
